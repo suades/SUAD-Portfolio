df15a0952611a66e7b34da1ae5d4216c6cb39be6
suades <suadesai17@gmail.com>
2024-06-01 04:08:35 -0500

    Fixed the Research cards

--- src/components/Research/ResearchCard.js
+++ src/components/Research/ResearchCard.js
@@ -9,31 +9,32 @@ import "../../style.css";
 function ResearchCard(props) {
   return (
     <Card className="project-card-view">
-      <Card.Body style={{ width:"100%"}}>
+      <Card.Body style={{ width: "100%" }}>
         <Card.Title>
           <span className="Title__wrapper">{props.title}</span>
         </Card.Title>
         <Card.Text className="scrollable-description">
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+        <div className="buttons-container">
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
           </Button>
-        )}
+          {"\n"}
+          {"\n"}
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}
+            >
+              <CgWebsite /> &nbsp;
+              {"Demo"}
+            </Button>
+          )}
+        </div>
         <div className="tags-container">
           {props.tags && props.tags.map((tag, index) => (
             <span key={index} className="tag">{tag}</span>
