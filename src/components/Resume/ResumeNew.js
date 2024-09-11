e4fbb3663bdb24abd8cba1658cd1c12c712b0b7e
Shlok Desai <suadesai17@gmail.com>
2024-09-11 12:36:55 -0400

    Put my Resume on AWS S3 for better efficiency

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Shlok Desai - Intern Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+const pdf = https://suad-portfolio-files.s3.us-east-2.amazonaws.com/Shlok+Desai+-+Intern+Resume.pdf
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
